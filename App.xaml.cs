fddb5c074919730c73fac4e5bb2084004ee9dc12
devCordain <pctkar@gmail.com>
2024-03-18 22:16:17 +0100

    init

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace path_of_filter;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
